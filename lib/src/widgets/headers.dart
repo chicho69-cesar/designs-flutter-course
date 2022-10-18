82009a34d29ad0efb260bbdb90f5592757702cd9
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-10-17 22:51:46 -0500

    Header con bordes redondeados

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -11,3 +11,25 @@ class HeaderCuadrado extends StatelessWidget {
     );
   }
 }
+
+class HeaderBordesRedondeados extends StatelessWidget {
+  const HeaderBordesRedondeados({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      /* No podemos usar las propiedades de diseño en un container cuando 
+      tenemos un BoxDecoration en este, ya que estas propiedades se pueden establecer
+      usando este BoxDecoration */
+      // color: const Color(0xff615AAB),
+      decoration: const BoxDecoration(
+        color: Color(0xff615AAB),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(70),
+          bottomRight: Radius.circular(70),
+        )
+      ),
+    );
+  }
+}
