c5e56b39981bee3ddc5c4a1aefeada7010e1b120
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-10-22 13:33:15 -0500

    Animated Square prepared to be animating

--- lib/src/pages/animations_page.dart
+++ lib/src/pages/animations_page.dart
@@ -13,3 +13,57 @@ class AnimationsPage extends StatelessWidget {
   }
 }
 
+class AnimatedSquare extends StatefulWidget {
+  const AnimatedSquare({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AnimatedSquare> createState() => _AnimatedSquareState();
+}
+
+class _AnimatedSquareState extends State<AnimatedSquare> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> rotation;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this, 
+      duration: const Duration(milliseconds: 4000)
+    );
+
+    rotation = Tween(
+      begin: 0.0,
+      end: 2.0
+    ).animate(controller);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const _Rectangle();
+  }
+}
+
+class _Rectangle extends StatelessWidget {
+  const _Rectangle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: const BoxDecoration(color: Colors.blue),
+    );
+  }
+}
